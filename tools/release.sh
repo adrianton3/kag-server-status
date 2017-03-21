3b4ac83d97f58c6e959fc379f30a963c0e3aaf7b
Adrian Toncean <adrianton3@gmail.com>
2017-03-21 18:15:50 +0100

    Fix release script

--- tools/release.sh
+++ tools/release.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 scripts/release
-cp release/kag_server_status.js out/kag_server_status.js
+mkdir tmp
+cp release/kag_server_status.js tmp/kag_server_status.js
 git checkout gh-pages
-rm -rf style
+cp tmp/kag_server_status.js out/kag_server_status.js
+rm -r style
 git checkout master -- style
 rm index.html
 git checkout master -- index.html
+rm -r tmp
